3fc05afc383f5f30672afc32fad657c3d94399d6
newjinny <syj9437@gmail.com>
2022-02-12 22:32:24 +0900

    19강까지 수강

---
+++ class-note/1_type-basic.ts
@@ -0,0 +1,32 @@
+//js 문자열 선언
+// var str = "hello";
+
+//ts 문자열 선언
+const str: string = "hello";
+
+//ts 숫자
+const num: number = 10;
+
+//ts 배열
+const arr: Array<number> = [1, 2, 3];
+const heroes: Array<string> = ["Capt", "Thor", "Hulk"];
+const items: number[] = [1, 2, 3];
+
+//ts 튜플
+const address: [string, number] = ["gangnam", 100];
+
+//ts 객체
+
+const obj: object = {};
+// const person: object = {
+//   name: 'capt',
+//   age: 100
+// };
+
+const person: { name: string; age: number } = {
+  name: "thor",
+  age: 100,
+};
+
+//ts 진위값
+const show: boolean = true;
