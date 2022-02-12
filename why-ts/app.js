354d19da85fa1eab80f6675036c682fae3f12d48
newjinny <syj9437@gmail.com>
2022-02-12 21:59:20 +0900

    first commit

--- why-ts/app.js
+++ why-ts/app.js
@@ -1,22 +1,51 @@
 // api url
-var url = 'https://jsonplaceholder.typicode.com/users/1';
+var url = "https://jsonplaceholder.typicode.com/users/1";
 
 // dom
-var username = document.querySelector('#username');
-var email = document.querySelector('#email');
-var address = document.querySelector('#address');
+var username = document.querySelector("#username");
+var email = document.querySelector("#email");
+var address = document.querySelector("#address");
 
 // user data
 var user = {};
+/**
+ * @typedef {object} Adress
+ * @property {string} street
+ * @property {string} city
+ *
+ */
+/**
+ *
+ * @typedef {object} User
+ * @property {string} name
+ * @property {string} email
+ * @property {Adress} address
+ */
+/**
+ *
+ * @returns {Promise<User>}
+ */
+function fetchUser() {
+  return axios.get(url);
+}
+fetchUser().then(function (res) {
+  res.address.city;
+});
 
 function startApp() {
-  axios
-    .get(url)
+  // axios
+  //   .get(url)
+  fetchiUser()
     .then(function (response) {
-      console.log(response);
+      //console.log(response);
       user = response.data;
       // TODO: 이름, 이메일, 주소 표시하기
+      console.log(user);
+      username.innerText = user[0].name;
+      email.innerText = user[0].email;
+      address.innerText = user[0].address.street;
     })
+
     .catch(function (error) {
       console.log(error);
     });
